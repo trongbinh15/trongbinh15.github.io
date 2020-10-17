6a01c0e99709576731f27a754b540fe5c112a436
Trong Binh Nguyen <trongbinhnguyen15@gmail.com>
2020-10-17 10:29:48 +0700

    splitting vendor bundle

--- webpack.common.js
+++ webpack.common.js
@@ -3,8 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { 
-    index: './src/index.js',
+  entry: {
+    index: path.resolve(__dirname, 'src/index'),
   },
   output: {
     filename: '[name].[hash].js',
@@ -49,25 +49,33 @@ module.exports = {
   optimization: {
     moduleIds: 'hashed',
     splitChunks: {
+      chunks: 'all',
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          priority: 20
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `vendor.${packageName.replace('@', '')}`;
+            },
+            chunks: 'all',
+            priority: 20
+          },
+          common: {
+            test: /[\\/]src[\\/]components[\\/]/,
+            chunks: "all",
+            reuseExistingChunk: true,
+            minSize: 0,
+            priority: 10
+          }
         },
-        common: {
-          test: /[\\/]src[\\/]components[\\/]/,
-          chunks: "all",
-          reuseExistingChunk: true,
-          minSize: 0,
-          priority: 10
-        }
       },
+      runtimeChunk: 'single',
     },
-    runtimeChunk: 'single',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
   }
-};
