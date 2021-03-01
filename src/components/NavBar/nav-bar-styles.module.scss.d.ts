7a174507b0e3275440ca9c3adcf40224abb52088
Trong Binh <trongbinhnguyen15@gmail.com>
2021-03-01 23:08:55 +0700

    error boundary

--- src/components/NavBar/nav-bar-styles.module.scss.d.ts
+++ src/components/NavBar/nav-bar-styles.module.scss.d.ts
@@ -3,7 +3,6 @@ export const darkColor: string;
 export const line1: string;
 export const line2: string;
 export const line3: string;
-export const logo: string;
 export const nav: string;
 export const navActive: string;
 export const navLink: string;
