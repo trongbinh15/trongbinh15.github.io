de88b9bf7160258d3c0560921620189f0951ef06
Trong Binh <binh.nguyen@klarda.com>
2023-06-04 10:43:54 +0700

    feat: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://trongbinh15.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
